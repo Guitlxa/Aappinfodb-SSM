b4f05cac3486b5b9a8a8043b0e09cd766eb5846c
Guitlxa <2934167181@qq.com>
2019-05-27 09:16:12 +0800

    分散

--- src/cn/app/constant/Constants.java
+++ src/cn/app/constant/Constants.java
@@ -2,5 +2,7 @@ package cn.app.constant;
 
 public class Constants {
 
-	public static final String USER_SESSION = "devUserService"; //静态常量 
+	public static final String USER_SESSION = "devUserService"; //静态常量
+	
+	public static final String USER_1 = "devUserSer"; //静态常量
 }
